37897c9d3a09d405ba2581e98fc9fa4e390484a1
tattack99 <62747925+tattack99@users.noreply.github.com>
2023-11-26 16:36:15 +0100

    added lanscape for detailScreen

--- WatherForecast/Views/DetailsScreen/DetailsScreen.swift
+++ WatherForecast/Views/DetailsScreen/DetailsScreen.swift
@@ -8,22 +8,54 @@
 import SwiftUI
 
 struct DetailsScreen: View {
+    @EnvironmentObject var viewModel: weather_forcastVM
     let location: FavoritLocation
     
-    
-    
+    @ViewBuilder
     var body: some View {
-        ZStack {
-            
-            BackgroundImage(imageName: "details-bg-light", overlayOpacity: 0.1)
-            ScrollView(showsIndicators: false){
-                VStack{
-                    TemperatureView(tempData: location.tempData, temp: Int(location.dayData[0].temp))
-                    HourListCard(hourlyData:location.hourData).padding(.vertical, -20)
-                    DayListCard(dailyData:location.dayData).padding(.bottom,30)
-                }
+        Group {
+            if viewModel.deviceOrientation.isPortrait {
+                portraitView
+            } else {
+                landscapeView
             }
         }
+    }
+    private var portraitView: some View {
+            ZStack {
+                BackgroundImage(imageName: "details-bg-light", overlayOpacity: 0.1)
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        TemperatureView(tempData: location.tempData, temp: Int(location.dayData[0].temp))
+                        HourListCard(hourlyData: location.hourData).padding(.vertical, -20)
+                        DayListCard(dailyData: location.dayData).padding(.bottom, 30)
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+        
+        private var landscapeView: some View {
+            // Your implementation for landscape orientation
+            ZStack {
+                BackgroundImage(imageName: "details-bg-light", overlayOpacity: 0.1)
+                    .position(backgroundPosition)
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        TemperatureView(tempData: location.tempData, temp: Int(location.dayData[0].temp))
+                        HourListCard(hourlyData: location.hourData).padding(.vertical, -20)
+                        DayListCard(dailyData: location.dayData).padding(.bottom, 30)
+                    }
+                    .padding(.all, 25) // This adds padding to accommodate for the notch
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+    
+    // Calculate the position based on the orientation
+    private var backgroundPosition: CGPoint {
+        viewModel.deviceOrientation.isPortrait ? CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2) :
+        CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.width / 2) // Example for landscape
     }
     
     
